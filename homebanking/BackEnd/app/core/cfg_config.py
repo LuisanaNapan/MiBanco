4c5f2376c467e26f0dcc147ec6ee9e9a2ce06edf
LuisanaNapan <166547382+LuisanaNapan@users.noreply.github.com>
2026-06-28 23:01:15 -0500

    nuevo

--- homebanking/BackEnd/app/core/cfg_config.py
+++ homebanking/BackEnd/app/core/cfg_config.py
@@ -5,12 +5,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
-    DATABASE_URL: str
+    DATABASE_URL: str = "postgresql://postgres:TU_CLAVE_DE_SUPABASE@...supabase.com:6543/postgres"
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 480
     PORT: int = 8002
-    CORS_ORIGINS: str = "http://localhost:5173"
+    CORS_ORIGINS: str = "http://localhost:5174,https://homebanking-fonrt-mibanco.vercel.app"
 
     @property
     def cors_origins_list(self) -> list[str]:
